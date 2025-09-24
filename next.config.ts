0a97cb4644913c1d8bb3c7392d875e89f46296e2
IFE <Ifeoladip@gmail.com>
2025-09-24 01:47:55 +0100

    Ready for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Allow builds to succeed even if ESLint errors exist
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
